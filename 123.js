845eba1fc68fb5c81b737021beac401d7ef5696b
choi ha young <gkdudk15@naver.com>
2023-05-23 19:26:16 +0900

    feat: test

--- 123.js
+++ 123.js
@@ -1,1 +1,12 @@
-console.log(123);
+// 삼항연산자
+
+// let food = '';
+// if (food === 'pizza') {
+//     console.log('🍕');
+// } else if (food === 'hamburger') {
+//     console.log('🍔');
+// } else (food === 'chicken') 
+//     console.log('😍');
+
+
+fruit === 'pizza' ? console.log('🍕') : console.log();
---
+++ 1234.js
@@ -0,0 +1,12 @@
+
+
+let food = '';
+if (food === 'pizza') {
+    console.log('🍕');
+
+} else  {
+    console.log('😍');
+}
+
+
+food === 'pizza' ? console.log('🍕') : console.log('😍');
